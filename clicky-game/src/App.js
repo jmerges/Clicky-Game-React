5587192a4c74520bdca7b825618d325702c550dc
jmerges bootcamp <jamesmerges1@gmail.com>
2021-02-22 16:03:13 -0800

    Set up the basic structure of the page

---
+++ clicky-game/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    order: [1,2,3,4,5,6,7,8,9,10,11,12]
+  }
+
+  newOrder = () => {
+    for (let i = this.state.order.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.state.order[i], this.state.order[j]] = [this.state.order[j], this.state.order[i]];
+    }
+  }
+
+  render() {
+    this.newOrder();
+    console.log(this.state.order);
+    return (
+    <div className="row">
+      <div className="col-3">1</div>
+      <div className="col-3">1</div>
+      <div className="col-3">1</div>
+      <div className="col-3">1</div>
+      <div className="col-3">1</div>
+      <div className="col-3">1</div>
+    </div>
+    );
+  }
+}
+
+export default App;
